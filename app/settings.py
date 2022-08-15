8b98b1d2d1a2a808ce8a12d02e83c3c1611a5bf8
meta-lyagushka <justlyagushka@gmail.com>
2022-08-15 17:06:25 +0300

    create docker image

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,10 @@
-DATABASE_URL = "postgresql://postgres_lyagushka:meta_pass@localHost:5432/postgres_lyagushka"
+DATABASE_URL = "postgresql://postgres_lyagushka:meta_pass@postgres:5432/postgres_lyagushka"
 
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
+    "http://todo.com",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
 ]
